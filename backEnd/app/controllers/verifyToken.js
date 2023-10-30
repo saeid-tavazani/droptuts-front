5bd840445cfc12a39575152941df75090881162f
saeid-tavazani <saeidtavazani@gmail.com>
2023-10-30 17:35:58 +0330

    fix send data verify

--- backEnd/app/controllers/verifyToken.js
+++ backEnd/app/controllers/verifyToken.js
@@ -5,7 +5,8 @@ exports.verifyToken = (req, res, next) => {
   const data = TokenService.decode(req.headers.authorization);
   selectAllUser(data.email).then((user) => {
     if (user[0].status == 1) {
-      res.send({ data, success: true, code: 200, message: "success" });
+      delete user[0].password;
+      res.send({ data: user[0], success: true, code: 200, message: "success" });
     } else {
       return res.status(401).send({
         status: "error",
