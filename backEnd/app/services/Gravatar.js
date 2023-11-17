1c06d82515033aed29ba766fa27b3302bde4e0e2
saeid-tavazani <saeidtavazani@gmail.com>
2023-11-17 20:35:02 +0330

    fix efault gravatar

--- backEnd/app/services/Gravatar.js
+++ backEnd/app/services/Gravatar.js
@@ -1,8 +1,5 @@
 const gravatar = require("gravatar");
 exports.gravatar = (email) => {
-  let picture = gravatar.url(email);
-  picture == "//www.gravatar.com/avatar/b9bc524123557af3b8e334486274513f"
-    ? (picture = null)
-    : picture;
+  const picture = gravatar.url(email, { d: "mm" });
   return { picture };
 };
