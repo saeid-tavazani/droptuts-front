b4e324b3e87ae40cdb56e8de745248a3735e8032
saeid tavazani <saeidtavazani@gmail.com>
2023-12-04 10:42:22 -0800

    fix avatar

--- frontEnd/src/components/NavBar/NavBarAvatar.jsx
+++ frontEnd/src/components/NavBar/NavBarAvatar.jsx
@@ -5,15 +5,45 @@ import axios from "../../assets/axios/";
 import jsonData from "../../assets/jsonData.json";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import { userInfo } from "../../store/userSlice";
 import { setToken } from "../../store/token";
-
+import { LiaUserEditSolid, LiaCashRegisterSolid } from "react-icons/lia";
+import { BiLogOut, BiUserCircle, BiLogIn } from "react-icons/bi";
+import { MdOutlineManageAccounts } from "react-icons/md";
 export default function NavBarAvatar({ classNames }) {
   const user = useSelector((state) => state.user.value);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const dataLogin = [
+    {
+      text: "ورود",
+      href: "/login",
+      icon: <BiLogIn size={20} />,
+    },
+    {
+      text: "ثبت نام",
+      href: "/register",
+      icon: <LiaCashRegisterSolid size={20} />,
+    },
+  ];
+  const dataUser = [
+    {
+      text: "خروج",
+      href: "/logout",
+      icon: <BiLogOut size={20} />,
+    },
+    {
+      text: "ویرایش حساب کاربری",
+      href: "/panel",
+      icon: <LiaUserEditSolid size={20} />,
+    },
+    {
+      text: "حساب کاربری",
+      href: "/panel",
+      icon: <MdOutlineManageAccounts size={20} />,
+    },
+  ];
+  const list = user ? dataUser : dataLogin;
   useEffect(() => {
     const cookie = Cookies.get(jsonData.cookieTokenName);
     if (user == null) {
@@ -26,13 +56,10 @@ export default function NavBarAvatar({ classNames }) {
               dispatch(userInfo(response.data.data));
               dispatch(setToken(cookie));
             } else {
-              alert(5555);
               Cookies.remove(jsonData.cookieTokenName);
             }
           })
           .catch((error) => {
-            alert(444);
-            console.log(error);
             Cookies.remove(jsonData.cookieTokenName);
           });
       }
@@ -44,11 +71,15 @@ export default function NavBarAvatar({ classNames }) {
       <Menu as="div" className="relative">
         <div>
           <Menu.Button className="relative flex rounded-full bg-blue-600 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 ">
-            <img
-              className="h-8 w-8 rounded-full"
-              src="https://droptuts.com/wp-content/themes/massive-dynamic/assets/img/logo-dark.png"
-              alt="user profile"
-            />
+            {user ? (
+              <img
+                className="h-8 w-8 rounded-full"
+                src={user.picture}
+                alt={user.name}
+              />
+            ) : (
+              <BiUserCircle />
+            )}
           </Menu.Button>
         </div>
         <Transition
@@ -61,45 +92,19 @@ export default function NavBarAvatar({ classNames }) {
           leaveTo="transform opacity-0 scale-95"
         >
           <Menu.Items className="absolute left-0 z-10 mt-2 w-48 origin-top-left transition-all rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <Menu.Item>
-              {({ active }) => (
+            {list.map((item, index) => (
+              <Menu.Item key={index}>
                 <Link
-                  to="#"
-                  className={classNames(
-                    active ? "bg-gray-100" : "",
-                    "block px-4 py-2 text-sm text-gray-700"
-                  )}
+                  to={item.href}
+                  className={
+                    " px-4 py-2 text-sm text-gray-700 flex w-full items-center gap-2"
+                  }
                 >
-                  Your Profile
+                  {item.icon}
+                  {item.text}
                 </Link>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <Link
-                  to="#"
-                  className={classNames(
-                    active ? "bg-gray-100" : "",
-                    "block px-4 py-2 text-sm text-gray-700"
-                  )}
-                >
-                  Settings
-                </Link>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <Link
-                  to="#"
-                  className={classNames(
-                    active ? "bg-gray-100" : "",
-                    "block px-4 py-2 text-sm text-gray-700"
-                  )}
-                >
-                  Sign out
-                </Link>
-              )}
-            </Menu.Item>
+              </Menu.Item>
+            ))}
           </Menu.Items>
         </Transition>
       </Menu>
