4c8fcab740f8fb2790bf3c7c9a842a54b39d99d0
saeid tavazani <saeidtavazani@gmail.com>
2023-12-10 01:19:48 -0800

    change input component

--- frontEnd/src/components/UI/Input.jsx
+++ frontEnd/src/components/UI/Input.jsx
@@ -1,9 +1,5 @@
-export default function Input({ type, className = "w-full", ...res }) {
-  return (
-    <input
-      {...res}
-      type={type}
-      className={`block rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${className}`}
-    />
-  );
+import { Input as InputM } from "@material-tailwind/react";
+
+export default function Input({ type, label, className = "w-full", ...res }) {
+  return <InputM {...res} color="blue" label={label} className={className} />;
 }
--- frontEnd/src/components/UI/ToggleBtn.jsx
+++ frontEnd/src/components/UI/ToggleBtn.jsx
@@ -5,7 +5,12 @@ export default function ToggleBtn({ ...res }) {
       className="flex  overflow-hidden items-center cursor-pointer "
     >
       <div {...res} className="relative shadow-sm">
-        <input type="checkbox" id="toggleB" className="sr-only m-0" />
+        <input
+          type="checkbox"
+          id="toggleB"
+          className="sr-only m-0"
+          defaultChecked
+        />
         <div className="block bg-gray-200 transition-all w-11 h-6 rounded-full"></div>
         <div className="dot absolute left-1.5 top-1 bg-white w-4 h-4 rounded-full transition-all"></div>
       </div>
