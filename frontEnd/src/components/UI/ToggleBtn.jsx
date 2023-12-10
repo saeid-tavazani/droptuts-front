9711c7828992a2384940d37eb1348ad04380df13
saeid tavazani <saeidtavazani@gmail.com>
2023-12-10 02:27:12 -0800

    change tyle

--- frontEnd/src/components/UI/ToggleBtn.jsx
+++ frontEnd/src/components/UI/ToggleBtn.jsx
@@ -1,6 +1,18 @@
 import { Switch } from "@material-tailwind/react";
 export default function ToggleBtn({ label, ...res }) {
   return (
-    <Switch color="blue" {...res} label={label} ripple={true} defaultChecked />
+    <Switch
+      color="blue"
+      className="h-full w-full"
+      containerProps={{
+        className: "w-11 h-6",
+      }}
+      circleProps={{
+        className: "before:hidden left-0.5 border-none",
+      }}
+      {...res}
+      label={label}
+      defaultChecked
+    />
   );
 }
