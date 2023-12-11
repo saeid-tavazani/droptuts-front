bf150d415223c674a66c5c64ddcdbeba110ce2cf
saeid tavazani <saeidtavazani@gmail.com>
2023-12-11 03:29:40 -0800

    fix bug type Input

--- frontEnd/src/components/UI/Input.jsx
+++ frontEnd/src/components/UI/Input.jsx
@@ -1,6 +1,6 @@
 import { Input as InputM } from "@material-tailwind/react";
 
-export default function Input({ type, label, className = null, ...res }) {
+export default function Input({ label, className = null, ...res }) {
   return (
     <div className={className}>
       <InputM {...res} color="blue" label={label} />
